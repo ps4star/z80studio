91708b4fdd87aabe5438271f29415b761b224717
ps4star <hoenn4life@gmail.com>
2020-11-22 17:44:57 -0500

    Add files via upload

--- pgmgmt.js
+++ pgmgmt.js
@@ -839,7 +839,7 @@ const defStr = `.cfg 00.type text
 jp _main
 
 @promptString:
-    .db "Welcome to Z80 Studio. Right now an audio port is configured to handle unsigned 8-bit wav data @22.05KHz, provided via file upload from the user (you). Use the first couple lines in the editor to change the audio format/settings if you want."
+    .db "Welcome to Z80 Studio. Right now an audio port is configured to handle unsigned 8-bit wav data, provided via file upload from the user (you). Use the first couple lines in the editor to change the audio format/settings if you want."
 
 _PromptS:
     ld a, (hl)
